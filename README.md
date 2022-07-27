a41522987c545ac29ed4588d3b36ff728b7b855b
sabbiramin <sabbiramin.cse11ruet@gmail.com>
2022-07-27 23:05:46 +0600

    Adding Basic Server Code

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
 ## Light-Redis
-A Redis Data Structure Based on Python Dict. 
+A Redis Like Data Structure (Server and Client) Based on Python Dict and Flask
 
-## Options
-1. File Based Database Storage accessed based on Queue.
-2. Server-Client model based on simple http server and multiple client. Here database lives in the server process.
-3. Server-Client using socket. 
-
-## File Based Database Storage - concerns, thoughts and points. 
-1. Lock the resource.
-2. Command insert into the Queue.
-3. Introduce `timelimit` for allowing each request equal treatment based on predefined time slot.
-4. Global monitor for executing commands in-parallel.
-5. How do we avoid Race conditions? Resource locking?
--- core.py
+++ core.py
@@ -2,22 +2,53 @@
 
 """
 author: S.M. Sabbir Amin
-date: 25 Jul 2022
+date: 27 Jul 2022
 email: sabbir.amin@goava.com, sabbiramin.cse11ruet@gmail.com
 
 """
-import json
 
-import codecs
+from flask import Flask, request
 
 
-class Database:
-    def __init__(self, db_file):
-        self.db_file = db_file
-        self.db = dict()
+class Server:
+    def __init__(self, host='localhost', port=5055, debug=True):
+        self.app = Flask(__name__)
+        self.db = {}
+        self.host = host
+        self.port = port
+        self.debug = debug
 
-    def init_database(self):
-        with codecs.open(self.db_file, 'w', 'utf-8') as f:
-            json.dumps(self.db, ensure_ascii=False, indent=1)
+        def _set(*args):
+            for arg in args:
+                print(arg)
+            key = args[0]
+            val = args[1]
+            self.db[key] = val
+            return 'OK'
 
+        def _get(*args):
+            key = args[0]
+            try:
+                return self.db[key]
+            except Exception as e:
+                return 'None'
+
+        self.command_handler = {'set': _set, 'get': _get}
+
+        def handler():
+            data = request.get_json()
+            cmd, key, value = data.get('cmd'), data.get('key'), data.get('value')
+            resp = self.command_handler[cmd](key, value)
+            print('resp:', resp)
+            return resp
+
+        self.app.add_url_rule("/", view_func=handler, methods=['POST'])
+
+    def run(self):
+        self.app.run(host=self.host, port=self.port, debug=self.debug)
+
+
+if __name__ == '__main__':
+    server = Server()
+    server.run()
 
